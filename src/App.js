92e5f7503228c97f26a0c6f7ebe3aa5a51985576
Jerry Lau <jerryzlau@gmail.com>
2017-12-04 12:44:58 -1000

    robot input and image generator done, need styling and add loading logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import RobotForm from './components/robot_form';
+import './css/robot_form.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1>
+        What is your character?
+      </h1>
         <RobotForm />
       </div>
     );
--- src/components/robot_form.js
+++ src/components/robot_form.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
 
 class RobotForm extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'jerry',
+      type: "?set=set1"
+    };
+  }
 
   update(field){
     return e => {
-      console.log(e.target.value);
+      this.setState({[field]: e.target.value});
     };
   }
 
   render() {
-    return <div>
-        <input onChange={this.update('name')}
-        className="robot-name" />
-        <select className="robot-types"
-        onChange={this.update('types')}>
-          <option value="?set=set1" default>Robot</option>
-          <option value="?set=set2">Monster</option>
-          <option value="?set=set3">Fancy Robot</option>
-          <option value="?set=set4">Cats</option>
-        </select>
+
+    const { name, type } = this.state;
+    const url = `https://robohash.org/${name}/${type}`;
+
+    return <div className="form">
+        <div className="input">
+          <label>Name:
+            <input onChange={this.update('name')}
+            className="robot-name" />
+          </label>
+          <select className="robot-types"
+          onChange={this.update('type')}>
+            <option value="?set=set1" default>Robot</option>
+            <option value="?set=set2">Monster</option>
+            <option value="?set=set3">Fancy Robot</option>
+            <option value="?set=set4">Cats</option>
+          </select>
+        </div>
+
+        <img src={url}/>
       </div>;
   }
 }
